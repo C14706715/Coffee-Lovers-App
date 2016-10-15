346b6c25ac68dd8ce95688ecc53265135428f595
Jake <C14706715@mydit.ie>
2016-10-15 15:12:21 +0100

    Adding Image scroll

--- Coffee_Lovers_App/app/src/main/java/com/example/jake/coffee_lovers_app/MainActivity.java
+++ Coffee_Lovers_App/app/src/main/java/com/example/jake/coffee_lovers_app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jake.coffee_lovers_app;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout layout =(LinearLayout) findViewById(R.id.GalleryLayoutID);
+        for(int i=0; i<5; i++){
+            ImageView iv = new ImageView(this);
+            iv.setId(i);
+            iv.setPadding(2, 2, 2, 2);
+            iv.setImageBitmap(BitmapFactory.decodeResource(
+                    getResources(), R.drawable.ic_launcher));
+            iv.setScaleType(ImageView.ScaleType.FIT_XY);
+            layout.addView(iv);
+        }
     }
 }
